710d49232cd682fc1fe2574e2067b7458df4e0e9
huntersteffner <huntersteffner@gmail.com>
2022-07-06 11:23:39 -0400

    created api key for last fm for testing of new api

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,22 @@ var bandButtonEl = document.getElementById("band-button");
 
 
 // Initialize and add the map
-const initMap = function () {
-    // The location of Atlanta
-    const atlanta = { lat: 33.74, lng: -84.38 };
-    // The map, centered at Atlanta
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: atlanta,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: atlanta,
-        map: map,
-    });
-}
+// const initMap = function () {
+//     // The location of Atlanta
+//     const atlanta = { lat: 33.74, lng: -84.38 };
+//     // The map, centered at Atlanta
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 10,
+//         center: atlanta,
+//     });
+//     // The marker, positioned at Uluru
+//     const marker = new google.maps.Marker({
+//         position: atlanta,
+//         map: map,
+//     });
+// }
 
-window.initMap = initMap;
+// window.initMap = initMap;
 
 var map = L.map('map').setView([33.74, -84.38], 12);
 var marker = L.marker([33.74, -84.38]).addTo(map);
